36c1545691383d5f6e553b0579a8552084419657
Kamalika Saha <111375654+Saha-Kamalika@users.noreply.github.com>
2024-01-29 11:10:13 +0530

    Create circularRotatedArray.cpp

---
+++ circularRotatedArray.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> rotate(int* arr, int n,int k){
+    if(k>n) k%=n;
+    vector<int> ans(n);
+    for(int i=0;i<k;i++) ans[i]=arr[n-k+i];
+    int index=0;
+    for(int i=k;i<n;i++) ans[i]=arr[index++];
+    return ans;
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++) cin>>arr[i];
+    int k;
+    cin>>k;
+    vector<int> ans= rotate(arr,n,k);
+    for(int i=0;i<n;i++) cout<<ans[i]<<" ";
+    return 0;
+}
